9039d0640a293a10127a0a6eb70f1d874f8e9299
Said Mghabghab <said@logicaldox.com>
2022-03-29 20:35:21 -0400

    initial gh-pages commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/saidmg/svelte-nestjs.git', // Update to point to your repository  
+        user: {
+            name: 'Said Mghabghab', // update to use your name
+            email: 'saidmghabghab@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
